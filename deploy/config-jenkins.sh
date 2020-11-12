558869ac4bdcf8a6bcfa5238bfb8acbdb382c010
LeoChiu1027 <lunayccleo@gmail.com>
2020-11-12 17:55:56 +0800

    Update config-jenkins.sh

--- deploy/config-jenkins.sh
+++ deploy/config-jenkins.sh
@@ -3,11 +3,22 @@
 # Jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get install dirmngr -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
+sudo apt-add-repository "deb http://repos.azul.com/azure-only/zulu/apt stable main"
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer -y
+sudo apt-get -y install zulu-8-azure-jdk
+sudo update-alternatives --set java /usr/lib/jvm/zulu-8-azure-amd64/jre/bin/java
+echo "JAVA_HOME='/usr/lib/jvm/zulu-8-azure-amd64'" >> /etc/profile
+echo "PATH=$PATH:$HOME/bin:$JAVA_HOME/bin" >> /etc/profile
+echo "export JAVA_HOME" >> /etc/profile
+echo "export PATH" >> /etc/profile
+sudo . /etc/environment
+echo $JAVA_HOME
 sudo apt-get install jenkins -y
+
+echo $PATH
 
 # Docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
@@ -18,13 +29,7 @@ sudo apt-get update
 sudo apt-get install docker-ce -y
 
 # Azure CLI
-sudo apt-get update
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get update
-sudo apt-get install azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Kubectl
 cd /tmp/
